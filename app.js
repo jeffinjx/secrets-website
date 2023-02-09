7cc37a498559c6fb2efa63136b493e5a6055ccd3
jeffinjx <jeffinjaison.9@gmail.com>
2023-02-10 02:02:32 +0300

    update port

--- app.js
+++ app.js
@@ -187,6 +187,6 @@ app.post("/login", function(req, res) {
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is now running in port 3000");
 });
